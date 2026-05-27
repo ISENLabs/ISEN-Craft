25fdb64e7c5bfd543a3ed927a8225dcf61e34bd0
Thomas Geffroy <thomas.geffroy@outlook.com>
2026-05-27 13:51:47 +0200

    fix(api-paper): merge bundled config as defaults on load

--- api-paper/src/main/java/fr/isen/paper/config/PaperConfigWrapper.java
+++ api-paper/src/main/java/fr/isen/paper/config/PaperConfigWrapper.java
@@ -7,6 +7,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class PaperConfigWrapper extends YamlConfigWrapper {
 
@@ -30,6 +33,12 @@ public class PaperConfigWrapper extends YamlConfigWrapper {
     public void load() throws IOException {
         saveDefaults();
         config = YamlConfiguration.loadConfiguration(new File(dataFolder, fileName));
+        InputStream resource = plugin.getResource(fileName);
+        if (resource != null) {
+            YamlConfiguration bundled = YamlConfiguration.loadConfiguration(
+                    new InputStreamReader(resource, StandardCharsets.UTF_8));
+            config.setDefaults(bundled);
+        }
     }
 
     @Override
